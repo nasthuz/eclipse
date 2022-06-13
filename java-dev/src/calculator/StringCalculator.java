0686df4297801de5f50a0b8f89448b5864d1541c
nasthuz <60304865+nasthuz@users.noreply.github.com>
2022-06-13 13:06:28 +0200

    Added step 4

--- java-dev/src/calculator/StringCalculator.java
+++ java-dev/src/calculator/StringCalculator.java
@@ -6,19 +6,43 @@ import java.util.List;
 
 public class StringCalculator {
 		public int add(String numbers){
-	    String[] nums = numbers.split(",|\r?\n|\r");
+	    String[] nums = setSplit(numbers);
 		List<Integer> data = new ArrayList<Integer>();
 		convertArrayIntoInt(nums,data);
 		int result = calculateSum(data);
-		
 		System.out.println("result = "  + result);
 		return result;
 	}
 	
 	
+	private String[] setSplit(String str) {
+		String delimeter;
+		String data;
+		String [] splitedNums = null;
+		if (str.startsWith("//")) {
+			int indexEnd = str.indexOf("\\");
+			int indexLine = str.indexOf("n");
+			delimeter = str.substring(2,indexEnd);
+			data=str.substring(indexLine+1, str.length()); 			
+			splitedNums = splitWithCustomDelimeter(data,delimeter);
+		}else {
+			System.out.println("The String is empty ");
+		}
+		return splitedNums;
+		}
+
+	
+	private String[] splitWithCustomDelimeter(String str,String delimeter) {
+		String [] splitedNums = str.split(delimeter);
+		for (String i: splitedNums) {
+			System.out.println("num = "  + i);
+			}
+		return splitedNums;
+	}
+
+
 	private static void convertArrayIntoInt(String[] stringData, List<Integer> intData) {
 		for (String i: stringData) {
-			System.out.println("num = "  + i);
 			if(i != null && !i.trim().isEmpty()) {
 				intData.add(Integer.parseInt(i));
 			}else {
@@ -30,9 +54,10 @@ public class StringCalculator {
 	private static int calculateSum(List<Integer> data) {
 		int result = 0;
 		for (int i: data) {
-			System.out.println("+num = "  + i);
 			result += i;
 		}
 		return result;
 	}
+	
+
 }
